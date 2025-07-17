89b8a1fe337aacf400341fff68ed5087d7e938b9
vferrand-smid <vferrand@smartphone-id.com>
2025-07-17 11:57:03 +0200

    preview cassé test ok

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,12 +4,15 @@ import {presentationTool} from 'sanity/presentation'
 import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
-
 import './styles/global.css'
+
+console.log('SANITY_PROJECT_ID:', process.env.SANITY_STUDIO_PROJECT_ID)
+console.log('SANITY_DATASET:', process.env.SANITY_STUDIO_DATASET)
+console.log('SANITY_API_TOKEN:', process.env.SANITY_STUDIO_API_TOKEN)
 
 export default defineConfig({
   name: 'default',
-  title: 'smartphone-id.test',
+  title: process.env.SANITY_STUDIO_TITLE,
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_DATASET!,
 
